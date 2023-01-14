e3870a1ab84c6cb4d02041ed724e46d60abcf801
Angus Chang <changangus2@gmail.com>
2023-01-13 20:34:34 -0500

    exercise 1 & 2

--- exercise/02-data-loading/app/routes/posts/index.tsx
+++ exercise/02-data-loading/app/routes/posts/index.tsx
@@ -1,7 +1,31 @@
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { Link } from "react-router-dom";
+import { prisma } from "~/db.server";
+
+async function getPosts() {
+  return prisma.post.findMany();
+}
+
+export const loader = async () => {
+  const posts = await getPosts();
+  return json({ posts });
+};
+
 export default function Posts() {
+  const { posts } = useLoaderData<typeof loader>();
   return (
     <main>
       <h1>Posts</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.slug}>
+            <Link to={post.slug} className="text-blue-600 underline">
+              {post.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
