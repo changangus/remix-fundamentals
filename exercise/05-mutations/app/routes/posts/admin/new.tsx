3f837014d9719d155bed01fc797c1fa765621820
Angus Chang <changangus2@gmail.com>
2023-01-17 22:19:11 -0500

    feat: exercise 5

--- exercise/05-mutations/app/routes/posts/admin/new.tsx
+++ exercise/05-mutations/app/routes/posts/admin/new.tsx
@@ -5,28 +5,69 @@
 // 4. call the createPost function from your post.model.ts
 // 5. redirect to "/posts/admin".
 
+import { Form, useActionData } from "@remix-run/react";
+import type { ActionArgs } from "@remix-run/server-runtime";
+import { redirect } from "@remix-run/server-runtime";
+import { json } from "@remix-run/node";
+import { createPost } from "~/models/post.server";
+import invariant from "tiny-invariant";
+
 const inputClassName = `w-full rounded border border-gray-500 px-2 py-1 text-lg`;
 
+export const action = async ({ request }: ActionArgs) => {
+  const formData = await request.formData();
+  const title = formData.get("title");
+  const slug = formData.get("slug");
+  const markdown = formData.get("markdown");
+
+  const errors = {
+    title: !title ? "Title is required" : "",
+    slug: !slug ? "Slug is required" : "",
+    markdown: !markdown ? "Markdown is required" : "",
+  };
+
+  if (Object.values(errors).some(Boolean)) {
+    return json(errors);
+  }
+
+  invariant(typeof title === "string", "title should be a string");
+  invariant(typeof slug === "string", "slug should be a string");
+  invariant(typeof markdown === "string", "markdown should be a string");
+
+  await createPost(title, slug, markdown);
+  return redirect("/posts/admin/");
+};
+
 export default function NewPost() {
+  const errors = useActionData<typeof action>();
   return (
     // 🐨 change this to a <Form /> component from @remix-run/react
     // 🐨 and add method="post" to the form.
-    <form>
+    <Form method="post">
       <p>
         <label>
           Post Title:{" "}
+          {errors?.title ? (
+            <em className="text-red-600">{errors.title}</em>
+          ) : null}
           <input type="text" name="title" className={inputClassName} />
         </label>
       </p>
       <p>
         <label>
           Post Slug:{" "}
+          {errors?.slug ? (
+            <em className="text-red-600">{errors.slug}</em>
+          ) : null}
           <input type="text" name="slug" className={inputClassName} />
         </label>
       </p>
       <p>
         <label htmlFor="markdown">Markdown: </label>
         <br />
+        {errors?.markdown ? (
+          <em className="text-red-600">{errors.markdown}</em>
+        ) : null}
         <textarea
           id="markdown"
           rows={8}
@@ -42,6 +83,6 @@ export default function NewPost() {
           Create Post
         </button>
       </p>
-    </form>
+    </Form>
   );
 }
